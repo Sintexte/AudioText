0ec2d4532877fefed64e25dea57c422c064f2d47
Sintexte <sintexte@gmail.com>
2021-04-26 07:11:36 +0200

    added some animations to logoff last commit @o@

--- front-end/src/components/mini/Logoff.js
+++ front-end/src/components/mini/Logoff.js
@@ -4,37 +4,35 @@ import {Spinner} from 'react-bootstrap'
 import { Redirect } from 'react-router';
 
 class Logoff extends React.Component{
-    timer_ = () => {
-        this.timeout = setInterval(() => {
-            if(this.state.dots.length <4){
-                this.setState({dots:this.state.dots+'.'})
-            }else{
-                this.setState({dots:' '})
-            }
-        }, 500  );  
-    }
+
     constructor(props){
         super(props)
         this.dots = ''
         this.state = {
             message:'Removing token',
             dots:' ',
-            redirect:false
+            redirect:false,
+            redirect_msg:false
         }
-        this.timer_()
     }
     
     componentDidMount(){
-        let cookies = new Cookies()
-        cookies.remove("token")
-        this.setState({message:'Redirecting',redirect:true})
+        setTimeout(()=>{
+            let cookies = new Cookies()
+            cookies.remove("token")
+            this.setState({message:'Redirecting',redirect_msg:true})
+        },150)
     }
 
     render(){
-        if(this.state.redirect){
-            return  <Redirect to="/" /> 
+        if(this.state.redirect_msg){
+            setTimeout(()=>{
+                this.setState({redirect:true}) 
+            },100)
         }
-
+        if(this.state.redirect){
+            return  <Redirect to="/" />
+        }
         return (
         <>
             <div style={{width:'30%',position:'absolute',marginTop:'40vh',marginLeft:'35vw'}}>
