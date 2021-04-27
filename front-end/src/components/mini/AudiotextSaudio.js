c2547b06bbd1da60d29eaec7b16959360850f156
Sintexte <sintexte@gmail.com>
2021-04-27 20:49:41 +0200

    Adding graphic Response to the file uploaded

--- front-end/src/components/mini/AudiotextSaudio.js
+++ front-end/src/components/mini/AudiotextSaudio.js
@@ -3,7 +3,13 @@
 import React from 'react'
 
 class AudiotextSaudio extends React.Component{
+    state={
+        default_msg:'Choose File',
+        actual_msg:'Choose File',
+        bad:false
+    }
     render(){
+        
         return (
             <>
                 <div style={{width:'70%', paddingTop:'20%',paddingLeft:'29%'}} >
@@ -11,12 +17,23 @@ class AudiotextSaudio extends React.Component{
                     <br />
                     <div className="input-group">
                         <div className="custom-file">
-                            <input type="file" className="custom-file-input" id="inputGroupFile01"
-                            aria-describedby="inputGroupFileAddon01" accept="audio/flac" />
-                            <label className="custom-file-label" for="inputGroupFile01">Choose file</label>
+                            <input type="file" onChange={(e)=>{
+                                if(e.target.files.length>=1){
+                                    this.setState({actual_msg:e.target.files[0].name})
+                                    if(e.target.files[0].type.toString() !== "audio/x-flac"){
+                                        this.setState({bad:true})
+                                    }else{
+                                        this.setState({bad:false})
+                                    }
+                                }
+                            }} className="custom-file-input" id="inputGroupFile01"
+                            aria-describedby="inputGroupFileAddon01" accept="audio/flac"  />
+                            <label className="custom-file-label" style={this.state.bad?{borderColor:'red'}:{border:''}} htmlFor="inputGroupFile01">
+                                {this.state.actual_msg}
+                            </label>
                         </div>
                         <div className="input-group-prepend">
-                            <input type='submit' className="input-group-text" id="inputGroupFileAddon01"  value='upload'/>
+                            <input type='submit' disabled={this.state.bad || (this.state.actual_msg === this.state.default_msg)?'disabled':''} className="input-group-text" id="inputGroupFileAddon01"  value='upload'/>
                         </div>
                     </div>
                 </div>
